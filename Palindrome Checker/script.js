1a8de7d6a6e125ba3533f893b5f54b4dc340a0db
Juan Camilo Herrera <juan8132juan@gmail.com>
2024-08-12 20:29:15 -0500

    Agregada funcionalidad

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,4 +1,26 @@
-const input = document.getElementById("text-input")
-const button = document.getElementById("check-btn")
-const result = document.getElementById("result")
+const input = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const result = document.getElementById("result");
 
+function isPalindrome(inputString){
+
+    const regText = inputString.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const invinputString = regText.split("").reverse().join("");
+    console.log(regText)
+    console.log(invinputString)
+    return (regText === invinputString) ? true : false;
+}
+
+function check(inputString){
+    if(inputString === ""){
+        alert("Please input a value");
+        return;
+    }else{
+        result.removeAttribute("hidden")
+        result.innerText = (isPalindrome(inputString)) ? `${input.value} is a palindrome` : `${input.value} is not a palindrome`;
+    }
+}
+
+button.addEventListener("click", () =>{
+    check(input.value)
+})
